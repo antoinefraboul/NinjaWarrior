01dc6e6ecbdb76549d90ff32f25ab4d060aa6ccd
antoinefraboul <antoine.fraboul@protonmail.com>
2019-04-23 09:36:30 +0200

    alpha

---
+++ NinjaWarrior/Assets/GameController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+
+    public Camera cam;
+
+    public Ennemy_AI[] tab_ai;
+
+    public Canvas myCanvas;
+
+    private Vector3 pos_cam;
+    private Quaternion rot_cam;
+    private bool reflexion_mode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos_cam = cam.transform.position;
+        rot_cam = cam.transform.rotation;
+
+        cam.transform.position = new Vector3(0, 10, 0);
+        cam.transform.rotation = Quaternion.Euler(new Vector3(90,0,0));
+
+        reflexion_mode = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (reflexion_mode)
+        {
+
+        }
+        
+        if (Input.GetMouseButton(1))
+        {
+            cam.transform.position = pos_cam;
+            cam.transform.rotation = rot_cam;
+            run();
+        }
+        
+    }
+
+    void run()
+    {
+        foreach (Ennemy_AI ai in tab_ai)
+        {
+            ai.initAI();
+        }
+    }
+
+    public void ReloadLevel()
+    {
+        SceneManager.LoadScene("Maze");
+    }
+}
---
+++ NinjaWarrior/Assets/Scripts/Ennemy_AI.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class Ennemy_AI : MonoBehaviour
+{
+
+    public NavMeshAgent agent;
+
+    public ThirdPersonCharacter character;
+
+    public GameObject target;
+
+    bool init= false;
+    bool regularSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent.updateRotation = false;
+        regularSpeed = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (init)
+        {
+            agent.SetDestination(target.transform.position);
+
+            if (agent.remainingDistance > agent.stoppingDistance)
+            {
+                character.Move(agent.desiredVelocity, false, false);
+            }
+            else
+            {
+                character.Move(Vector3.zero, false, false);
+            }
+        }
+        
+    }
+
+    public void initAI()
+    {
+        init = true;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "speed_down" && regularSpeed)
+        {
+            agent.speed = agent.speed/3;
+            regularSpeed = false;
+        }
+    }
+
+}
